60b31e26255cb26c4571d15925957ce8220bc054
James Jarrett <jjarrett21@gmail.com>
2019-06-13 14:06:31 -0500

    added react router

--- src/components/Root.js
+++ src/components/Root.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import {Provider} from 'react-redux';
 import App from './App';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
 
 const Root = ({store}) => (
     <Provider store={store}>
-        <App />
+        <Router>
+            <Route exact path="/" component={App} />
+        </Router>
     </Provider>
 );
+
+Root.propTypes = {
+    store: PropTypes.object.isRequired,
+};
 
 export default Root;
 
